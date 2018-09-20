67bfcf1401dfcd84962ed771e8a93a48c630b369
boomboompower <boomboompower@users.noreply.github.com>
2018-09-20 17:15:22 +1000

    Update AddonExample.java

--- src/main/java/com/example/AddonExample.java
+++ src/main/java/com/example/AddonExample.java
@@ -2,6 +2,7 @@ package com.example;
 
 import cc.hyperium.event.*;
 import cc.hyperium.internal.addons.IAddon;
+
 import net.minecraft.client.Minecraft;
 
 public class AddonExample implements IAddon {
@@ -9,12 +10,13 @@ public class AddonExample implements IAddon {
     @Override
     public void onLoad() {
         System.out.println("Successfully loaded Addon!");
+        
         EventBus.INSTANCE.register(this);
     }
 
     @InvokeEvent
     private void onChatMessage(ChatEvent event) {
-        if(event.getChat().getUnformattedText().contains("secret message!")) {
+        if (event.getChat().getUnformattedText().contains("secret message!")) {
             Minecraft.getMinecraft().toggleFullscreen();
         }
     }
